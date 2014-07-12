bb561e00b1ac0bf0baec12b1efa4fde7335bd40c
Doug Hellmann <doug@doughellmann.com>
2014-07-12 15:16:24 -0400

    support --dry-run for 'tags canonicalize'

--- linkscrubber/cmd/tags.py
+++ linkscrubber/cmd/tags.py
@@ -15,9 +15,10 @@ class TagsCanonicalize(command.Command):
         all_tags = client.tags()
         self.log.info('Found %d separate tags', len(all_tags))
 
-        tag_names = set(t['name'] for t in all_tags)
+        tag_names = (t['name'] for t in all_tags)
         for tag in tag_names:
             c_tag = tag.lower()
             if c_tag != tag:
                 self.log.info('Rename "%s" to "%s"', tag, c_tag)
-                client.rename_tag(tag, c_tag)
+                if not self.app.options.dry_run:
+                    client.rename_tag(tag, c_tag)
